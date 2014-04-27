1bae4f5caa69c7a687df7f2f657720610a17b7e0
Adam Borowski <kilobyte@angband.pl>
2014-04-27 22:33:34 +0200

    getopt and --test

--- main.cc
+++ main.cc
@@ -1,9 +1,36 @@
-#include <locale.h>
 #include "adalyah.h"
+#include <locale.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "test.h"
 
-int main()
+static struct option options[] =
+{
+    { "test",            2, NULL, 't', },
+    {0},
+};
+
+static void parse_options(int argc, char **argv)
+{
+    while (1)
+        switch (getopt_long(argc, argv, "t::", options, NULL))
+        {
+        case -1:
+            return;
+        case 't':
+            run_tests(optarg);
+            exit(0);
+        default:
+            // getopt gave the error message already
+            exit(1);
+        }
+}
+
+int main(int argc, char **argv)
 {
     setlocale(LC_ALL, "");
+    parse_options(argc, argv);
 
     return 0;
 }
---
+++ test.cc
@@ -0,0 +1,6 @@
+#include "adalyah.h"
+#include "test.h"
+
+void run_tests(const char* arg)
+{
+}
